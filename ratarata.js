ab310c210a5913a3c8827adfb56918982cca7388
ninjaa <alkhudrisaid83@gmail.com>
2020-09-11 07:24:29 +0700

    first commit

---
+++ ratarata.js
@@ -0,0 +1,11 @@
+var nilaisantri = [ 5,9,6,7,9,8,10,7,8];
+var total = 0;
+var isinilai = nilaisantri.length;
+
+
+for (i=0; i < isinilai; i++){
+    console.log(i);
+    total += nilaisantri[i]
+
+}
+document.write(total / isinilai);
